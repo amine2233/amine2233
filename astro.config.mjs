3c485a4b43be89f365c85f80090a1fe796c1d1ed
Amine Bensalah <amine.bensalah@intech-consulting.fr>
2024-12-30 20:39:01 +0100

    feat: add pnpm astro add astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://amine2233.github.io",
   base: "amine2233",
   trailingSlash: "always",
-  integrations: [tailwind()],
+  integrations: [tailwind(), icon()]
 });
