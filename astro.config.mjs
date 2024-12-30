145d667f9f3f73934a166b9a79bd5150e18884ad
Amine Bensalah <amine.bensalah@intech-consulting.fr>
2024-12-30 20:57:17 +0100

    feat: update
    feat: update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
-import icon from "astro-icon";
-
 // https://astro.build/config
 export default defineConfig({
   site: "https://amine2233.github.io",
   base: "amine2233",
   trailingSlash: "always",
-  integrations: [tailwind(), icon()]
+  integrations: [tailwind()],
 });
