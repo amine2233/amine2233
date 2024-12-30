6dd076f97b339a41c25835bb78064398180faeeb
Amine Bensalah <amine.bensalah@intech-consulting.fr>
2024-12-30 11:43:48 +0100

    feat: update cv and add color
    feat: update cv
    
    feat: update deploy workflow
    
    feat: update deploy workflow
    
    feat: update to try to work
    
    feat: add pnpm astro add astro-icon
    
    feat: update
    
    feat: update
    
    feat: update
    
    feat: update
    
    feat: update
    
    feat: update
    
    feat: update
    
    feat: update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://amine2233.github.io",
+  base: "amine2233",
+  trailingSlash: "always",
   integrations: [tailwind()],
 });
